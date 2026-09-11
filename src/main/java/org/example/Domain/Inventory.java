462df3017804a109c188e910175878c48cf60f43
Chetanya729 <chetanyabansal410@gmail.com>
2026-09-11 17:48:04 +0530

    Race condition fixed in

--- src/main/java/org/example/Domain/Inventory.java
+++ src/main/java/org/example/Domain/Inventory.java
@@ -1,4 +1,29 @@
 package org.example.Domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
+    private final Map<MenuItems, Integer> stock = new HashMap<>();
+
+    public Inventory() {
+        stock.put(MenuItems.PIZZA, 8);
+        stock.put(MenuItems.BURGER, 10);
+        stock.put(MenuItems.PASTA, 6);
+        stock.put(MenuItems.SANDWICH, 7);
+    }
+
+    public synchronized boolean reserve(MenuItems Item, int quantity){
+        int available = stock.get(Item);
+        if(available>quantity) {
+            stock.put(Item, available - 1);
+            return true;
+        }
+        return false;
+    }
+
+
+    public int getStock(MenuItems Item) {
+        return stock.get(Item);
+    }
 }
